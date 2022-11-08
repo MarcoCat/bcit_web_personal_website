b5e75a9b92bfbfae3ad5069d0c9dbf1698788a4f
Marco Laptop <mpoon25@my.bcit.ca>
2022-11-08 12:29:44 -0800

    creates constant array

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+courseList = [
+    { code: "ACIT 1620", name: "Web Fundamental Technologies"}, 
+    { code: "ACIT 1515", name: "Scripting for IT"}, 
+    { code: "MATH 1310", name: "Technical Math for IT"}
+    ];
