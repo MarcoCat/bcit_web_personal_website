5127efe592f99bd58a77f77e26a75f268ac1a4b1
Marco Laptop <mpoon25@my.bcit.ca>
2022-11-08 12:30:30 -0800

    Takes 4 digit number input from user

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
-courseList = [
+const courseList = [
     { code: "ACIT 1620", name: "Web Fundamental Technologies"}, 
     { code: "ACIT 1515", name: "Scripting for IT"}, 
     { code: "MATH 1310", name: "Technical Math for IT"}
     ];
+
+do{
+        user_input = prompt('Enter a 4 digit code: ')
+    } while(isNaN(Number(user_input)) || user_input.length !== 4);
