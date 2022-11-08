4a4bd0bb9f2c91c2286f6d1da8338fd7f0ecbb27
Marco Laptop <mpoon25@my.bcit.ca>
2022-11-08 12:32:51 -0800

    Logs if the code is found, otherwise adds to list

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,25 @@
-const courseList = [
+courseList = [
     { code: "ACIT 1620", name: "Web Fundamental Technologies"}, 
     { code: "ACIT 1515", name: "Scripting for IT"}, 
     { code: "MATH 1310", name: "Technical Math for IT"}
     ];
+    
+let user_input = null;
+let flag = false
 
 do{
-        user_input = prompt('Enter a 4 digit code: ')
-    } while(isNaN(Number(user_input)) || user_input.length !== 4);
+    user_input = prompt('Enter a 4 digit code: ')
+} while(isNaN(Number(user_input)) || user_input.length !== 4);
+
+for(course of courseList){
+    if (course['code'].includes(user_input)){
+        console.log(`Yes I am taking the course: ${course['code']} – ${course['name']}`)
+        flag = true
+    }
+}
+
+if (flag == false){
+    courseList.push({code: user_input, name: null})
+}
+
+console.log('Success')
